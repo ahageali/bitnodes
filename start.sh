c061d0c7af3a3c3432905de08f94f92b3b2b7466
Abdul Hage-ali <abdul.hageali@gmail.com>
2017-11-16 07:29:22 -0500

    Remove testnet

--- start.sh
+++ start.sh
@@ -27,23 +27,3 @@ python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.1.out 2>&1 &
 python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.2.out 2>&1 &
 python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.3.out 2>&1 &
 
-# --- bitcoin testnet3: 0b110907 (db = 1) ---
-python -u crawl.py conf/crawl.0b110907.conf master > log/crawl.0b110907.master.out 2>&1 &
-python -u crawl.py conf/crawl.0b110907.conf slave > log/crawl.0b110907.slave.1.out 2>&1 &
-
-python -u ping.py conf/ping.0b110907.conf master > log/ping.0b110907.master.out 2>&1 &
-python -u ping.py conf/ping.0b110907.conf slave > log/ping.0b110907.slave.1.out 2>&1 &
-
-python -u resolve.py conf/resolve.0b110907.conf > log/resolve.0b110907.out 2>&1 &
-
-python -u export.py conf/export.0b110907.conf > log/export.0b110907.out 2>&1 &
-
-python -u seeder.py conf/seeder.0b110907.conf > log/seeder.0b110907.out 2>&1 &
-
-python -u pcap.py conf/pcap.0b110907.conf > log/pcap.0b110907.1.out 2>&1 &
-python -u pcap.py conf/pcap.0b110907.conf > log/pcap.0b110907.2.out 2>&1 &
-python -u pcap.py conf/pcap.0b110907.conf > log/pcap.0b110907.3.out 2>&1 &
-
-# --- bitcoin cash mainnet: e3e1f3e8 (db = 2) ---
-#python -u crawl.py conf/crawl.e3e1f3e8.conf master > log/crawl.e3e1f3e8.master.out 2>&1 &
-#python -u crawl.py conf/crawl.e3e1f3e8.conf slave > log/crawl.e3e1f3e8.slave.1.out 2>&1 &
